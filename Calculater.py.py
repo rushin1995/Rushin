d41eaf7cdc615cd3dd8b8c9a5e3ff1178da33ce9
rushin <rushin1995>
2022-12-08 15:30:13 +0530

    first commit

--- Calculater.py.py
+++ Calculater.py.py
@@ -20,43 +20,14 @@ def divide(a, b):
  
 print("SIMPLE CALCULATOR")  
   
-# using the while loop to print menu list  
-while True:  
-    print("\nMENU")  
-    print("1. ADDITION")  
-    print("2. SUBTRACTION")  
-    print("3. MULTIPLICATION")  
-    print("4. DIVISION")  
-    print("5. EXIT")  
-    choice = int(input("\nEnter the Choice: "))  
   
-# using if-elif-else statement to pick different options  
-    if choice == 1:  
-        print( "\nADDITION\n")  
-        a = int( input("First Number: "))  
-        b = int( input("Second Number: "))  
-        add(a, b)  
+ 
+ 
+add(5, 5)  
+subtract(8, 8)  
+multiply(7,2)      
+divide(10, 2)  
   
-    elif choice == 2:  
-        print( "\nSUBTRACTION\n")  
-        a = int( input("First Number: "))  
-        b = int( input("Second Number: "))  
-        subtract(a, b)  
-  
-    elif choice == 3:  
-        print( "\nMULTIPLICATION\n")  
-        a = int( input("First Number: "))  
-        b = int( input("Second Number: "))  
-        multiply(a, b)  
-  
-    elif choice == 4:  
-        print( "\nDIVISION\n")  
-        a = int( input("First Number: "))  
-        b = int( input("Second Number: "))  
-        divide(a, b)  
-  
-    elif choice == 5:  
-        break  
+        
       
-    else:  
-        print( "Please Provide a valid Input!")  
+      
